4d25e9a02400420032b70c67b4d35d202b122047
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-23 16:27:49 +0900

    Added get method
    app.get("/")는 브라우저가 서버에게 root를 GET 요청했고 서버가 응답했다.
    
    브라우저가 사용자 대신에 서버에 요청을 보낸다.
    
    서버가 브라우저의 요청을 받았는데 응답하지 않으면 브라우저는 계속 기다린다.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import express from "express";
 const PORT = 8081;
 const app = express();
 
+app.get("/", (req, res, next) => {
+    res.send("here")
+})
 const handleListening = ( ) => {
     console.log("8081 is working")
 }
