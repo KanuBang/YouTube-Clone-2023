1545c4919c65f1d09835ddf5139af47a81baee60
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-23 17:29:15 +0900

    Added middlewares
    미들웨어는 요청과 응답 사이에 위치.
    미들웨어는 응답하는 함수가 아니라 작업을 다음 함수에 넘기는 함수다.
    next가 있다면 미들웨어다
    
    handler = controller = middleware
    이거는 나머지 매개변수로 저장되기에
    개수가 유한하지 않다

--- src/server.js
+++ src/server.js
@@ -2,15 +2,21 @@ import express from "express";
 const PORT = 8081;
 const app = express();
 
+const fake = (req, res, next) => {
+    console.log(`someonde is going to: ${req.url}`)
+    next()
+}
+
 const handleHome =  (req, res, next) => {
-    return res.send({msg: "home"})
+    return res.send({msg: "hello"})
 }
 
 const handleLogin = (req, res, next) => {
     return res.end("<h1>Login</h1>")
 }
 
-app.get("/", handleHome)
+
+app.get("/", fake, handleHome)
 app.get("/login", handleLogin)
 
 const handleListening = ( ) => {
