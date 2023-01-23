ef497002f600374acf2a7ce1cf2e94dc52557a15
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-23 17:03:25 +0900

    Added a simple response
    브라우저가 root 페이지와 로그인 페이지를 가져달라는 요청을 서버에게 보낸다. 그리고 서버는 요청에 응답한다
    
    express가 첫 번째로 요청 객체, 두 번째로 응답 객체를 제공한다. 이름은 뭐든 상관없다
    
    /는 root 혹은 첫 페이지를 의미한다

--- src/server.js
+++ src/server.js
@@ -2,11 +2,20 @@ import express from "express";
 const PORT = 8081;
 const app = express();
 
-app.get("/", (req, res, next) => {
-    res.send("here")
-})
+const handleHome =  (req, res, next) => {
+    return res.end("Home")
+}
+
+const handleLogin = (req, res, next) => {
+    return res.end("Login")
+}
+
+app.get("/", handleHome)
+app.get("/login", handleLogin)
+
 const handleListening = ( ) => {
     console.log("8081 is working")
 }
+
 app.listen(PORT, handleListening)
 
