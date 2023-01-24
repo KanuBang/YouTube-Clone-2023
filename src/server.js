622fd1d681e8c57b46909329b21b5bfe672d1867
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-24 14:18:58 +0900

    Added morgan
    1. morgan은 next를 호출하기에 미들웨어이다.
    2. morgan은 미들웨어를 리턴한다.
    3. morgan은 설정이 필요한 미들웨어다

--- src/server.js
+++ src/server.js
@@ -1,40 +1,19 @@
 import express from "express";
+import morgan from "morgan";
 const PORT = 8081;
 const app = express();
 
-const loggerMiddleware = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`)
-    next()
-}
 
-const privateMiddleware = (req, res, next) => {
-    const url = req.url
-    
-    if(url == "/protected") {
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    
-    console.log("Allowed, you many continue")
-    next()
-}
-
-const handleHome =  (req, res, next) => {
-    return res.send("<h1>Allowed</h1>")
-}
-
-const handleProtected =  (req, res, next) => {
-    return res.send("<h1>Protected</h1>")
-}
-
-const handleLogin = (req, res, next) => {
+const logger = morgan("dev")
+const handleHome = (req, res, next) => {
+    console.log("response")
     return res.end("<h1>Login</h1>")
 }
 
-app.use(loggerMiddleware)
-app.use(privateMiddleware)
+app.use(logger)
 app.get("/", handleHome)
-app.get("/protected", handleProtected)
-app.get("/login", handleLogin)
+
+
 
 const handleListening = ( ) => {
     console.log("8081 is working")
