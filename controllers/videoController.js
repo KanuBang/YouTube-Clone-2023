7e929df02201af7253ca235115f5f0305ed34e59
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-24 17:50:08 +0900

    Used a parameter, req.params / Updated day2Recap

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,9 @@
 export const trending = (req, res) => res.send("Trending");
-export const see = (req, res) => res.send("Watch");
+
+export const see = (req, res) => {
+    return res.send(`Watch Video #${req.params.id}`)   
+}
+
 export const edit = (req, res) => res.send("Edit Video");
 export const search = (req, res) => res.send("Search")
 export const upload = (req, res) => res.send("Upload")
