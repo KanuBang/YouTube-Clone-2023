f9d04f51e8689851456992a13da5cec5de05d085
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-24 17:39:53 +0900

    Added more routers, controllers

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,8 +1,13 @@
 import express from "express";
+import { deleteVideo, upload,see,edit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("watch Videos")
-videoRouter.get("/watch", handleWatchVideo);
+
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit );
+videoRouter.get("/id/delete",deleteVideo);
+
 
 export default videoRouter;
 
