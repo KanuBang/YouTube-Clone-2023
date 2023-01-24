05116ae129c66a7eb251bf7040368ff796f3d8f7
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-24 15:22:19 +0900

    Added Router & README.md, Updated day2Recap

---
+++ README.md
@@ -0,0 +1,20 @@
+# Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+=> 글로벌 라우터: 홈에서 바로 갈 수 있는 페이지들
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+=> 유저 라우터: 유저에서 갈 수 있는 페이지들
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete A
+=> 비디오 라우터: 비디오에서 갈 수 있는 페이지들
+
+Routing refers to how an application’s endpoints (URIs) respond to client requests.
--- src/server.js
+++ src/server.js
@@ -2,22 +2,26 @@ import express from "express";
 import morgan from "morgan";
 const PORT = 8081;
 const app = express();
-
-
 const logger = morgan("dev")
-const handleHome = (req, res, next) => {
-    console.log("response")
-    return res.end("<h1>Login</h1>")
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home")
+globalRouter.get("/", handleHome)
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User")
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("watch Videos")
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () => {
+    console.log(`${PORT} is listening`)
 }
-
-app.use(logger)
-app.get("/", handleHome)
-
-
-
-const handleListening = ( ) => {
-    console.log("8081 is working")
-}
-
 app.listen(PORT, handleListening)
 
