4c150005f6872a48f647e357f2ac39d04ef3c275
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-26 16:04:35 +0900

    Added Upload page

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,18 @@
 import express from "express";
 import { 
     deleteVideo, 
-    upload,
     watch,
     getEdit,
-    postEdit 
+    postEdit,
+    getUpload,
+    postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
 
