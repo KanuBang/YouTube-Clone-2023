6ae9993efcf7417acc49fd33fd5ec6c4a11e5cf6
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-26 15:10:10 +0900

    Used relative, absolute route, GET & POST, teranry operator

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { deleteVideo, upload,see,edit } from "../controllers/videoController";
+import { 
+    deleteVideo, 
+    upload,
+    watch,
+    getEdit,
+    postEdit 
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit );
-videoRouter.get("/id(\\d+)/delete",deleteVideo);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id(\\d+)/edit", getEdit );
+videoRouter.post("/:id(\\d+)/edit", postEdit );
 
 
 export default videoRouter;
