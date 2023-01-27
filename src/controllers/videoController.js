6cf1f838f0e55db9aee1868181eda806f92bc7c0
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-27 15:25:53 +0900

    Updated edit page with model method

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -22,18 +22,43 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
     const {id} = req.params;
     const video = await Video.findById(id);
-    console.log(video)
+    console.log(video) //just test
+    if(!video) {
+      return res.render("404", {pageTitle: "NOT FOUND"})
+    }
+    console.log("hereadsf")
+
     return res.render('watch', {pageTitle: video.title, video:video})   
 }
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
+  console.log("here")
   const {id} = req.params;
-  return res.render('edit', {pageTitle: `Editing`})
+  const video = await Video.findById(id);
+  console.log("5")
+
+  if(!video) {
+    console.log("23452345")
+    return res.render("404", {pageTitle: "NOT FOUND"})
+  }
+  console.log("hereasdfsdfafd")
+  return res.render('edit', {pageTitle: `Edit: ${video.title}`, video})
 }
 
-export const postEdit = (req, res ) => {
+export const postEdit = async (req, res ) => {
   const id = req.params.id;
-  const title = req.body.title;
+  const {title, description, hashtags} = req.body;
+  const video = await Video.findById(id);
+  if(!video) {
+    return res.render("404", {pageTitle: "NOT FOUND"})
+  }
+
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`))
+  await video.save()
   return res.redirect(`/videos/${id}`);
 };
 
@@ -50,7 +75,9 @@ export const postUpload = async (req,res) => {
       description,
       //createdAt:"asdfsadf", // error발생  -> catch문으로 이동
       createdAt: Date.now(),
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: hashtags
+                  .split(",")
+                  .map((word) => (word.startsWith("#") ? word : `#${word}`)),
       meta: {
         views: 0,
         rating: 0,
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -11,7 +11,7 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id[0-9a-f]{24}/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
