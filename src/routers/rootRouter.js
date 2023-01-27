243e9a7ed880be26739b774927b94f2f26f163c0
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-27 23:21:44 +0900

    Saved Join data

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,8 +1,7 @@
 import express from "express";
 import {getJoin, postJoin, login} from "../controllers/userController";
 import {home, search} from "../controllers/videoController";
-const
-rootRouter = express.Router();
+const rootRouter = express.Router();
 
 rootRouter.get("/", home)
 rootRouter.route('/join').get(getJoin).post(postJoin)
