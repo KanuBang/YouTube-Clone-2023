e43e293428b15191499c1a458e73966cc9f96e2d
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-01-27 13:50:07 +0900

    Used findById and regular exprerssion to find a video by ID

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -19,9 +19,11 @@ export const home = async (req, res) => {
     res.send("it will be ingnored. That's because there is return")
 }
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
     const {id} = req.params;
-    return res.render('watch', {pageTitle: `Watching`})   
+    const video = await Video.findById(id);
+    console.log(video)
+    return res.render('watch', {pageTitle: video.title, video:video})   
 }
 
 export const getEdit = (req, res) => {
