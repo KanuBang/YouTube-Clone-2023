749a77f27d30848c095d0d7cb43d4242e1e43fb3
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-02-17 17:46:34 +0900

    timeline

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -5,7 +5,7 @@ const time = document.getElementById("time")
 const volumeRange = document.getElementById("volume")
 const currentTime = document.getElementById("currentTime")
 const totalTime = document.getElementById("totalTime")
-
+const timeline = document.getElementById("timeline")
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
@@ -44,15 +44,24 @@ const handleVolumeChange = (event) => {
 }
 
 const formatTime = (seconds) => {
-    return new Date (seconds * 1000).toISOString().substring(14, 19);
+    return new Date (seconds * 1000).toISOString().substring(11, 19);
 }
 
 const handleLoadedMetaData = () => {
     totalTime.innerText = formatTime(Math.floor(video.duration))
+    timeline.max = Math.floor(video.duration)
 }
 
 const handleTimeUpdate = () => {
     currentTime.innerText = formatTime(Math.floor(video.currentTime))
+    timeline.value = Math.floor(video.currentTime)
+}
+
+const handleTimelineChange = (event) => {
+    const {
+        target: {value},
+    } = event;
+    video.currentTime = value;
 }
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -60,3 +69,4 @@ muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetaData)
 video.addEventListener("timeupdate", handleTimeUpdate)
+timeline.addEventListener("input", handleTimelineChange)
